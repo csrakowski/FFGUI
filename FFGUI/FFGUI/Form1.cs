ddfeff2fae1324b6e4d8bca45e16bfa2510b92ce
Christiaan Rakowski <cs.rakowski@gmail.com>
2014-03-03 20:20:36 +0100

    added snippet for thumbnail generation

--- FFGUI/FFGUI/Form1.cs
+++ FFGUI/FFGUI/Form1.cs
@@ -18,7 +18,7 @@ namespace FFGUI
 			string ffmpeg = ConfigurationManager.AppSettings["FFMPEG_PATH"];
 			if(!File.Exists(ffmpeg))
 			{
-                SimpleLogger.LogMessage("No FFMpeg found, prompting user");
+				SimpleLogger.LogMessage("No FFMpeg found, prompting user");
 				MessageBox.Show(this, Resources.Form1_Form1_This_application_uses_FFMPEG_for_it_s_conversion__Please_select_where_ffmpeg_exe_can_be_found, Resources.Form1_Form1_Missing_FFMPEG, MessageBoxButtons.OK, MessageBoxIcon.Error);
 				var dialog = new OpenFileDialog();
 				var result = dialog.ShowDialog();
@@ -34,26 +34,26 @@ namespace FFGUI
 					Application.Exit();
 				}
 			}
-            SimpleLogger.LogMessage(String.Format("Using ffmpeg at: \"{0}\"", ffmpeg));
+			SimpleLogger.LogMessage(String.Format("Using ffmpeg at: \"{0}\"", ffmpeg));
 
-            saveFileDialog1.Filter = Resources.FileFormats;
-            saveFileDialog1.DefaultExt = "MP4";
+			saveFileDialog1.Filter = Resources.FileFormats;
+			saveFileDialog1.DefaultExt = "MP4";
 
-            conversionFormats.Items.AddRange(new[] {
-                "MP4",
-                "MP3",
-                "MPEG",
-                "AVI",
-                "FLV"
-            });
-            conversionFormats.SelectedIndex = 0;
+			conversionFormats.Items.AddRange(new[] {
+				"MP4",
+				"MP3",
+				"MPEG",
+				"AVI",
+				"FLV"
+			});
+			conversionFormats.SelectedIndex = 0;
 
-		    foreach (var preset in EncodingOptions.Presets)
-		    {
-		        presets.Items.Add(preset.PresetName);
-		    }
-		    presets.SelectedIndex = 1;
-		    //SetEncodingOptions(EncodingOptions.Custom720);
+			foreach (var preset in EncodingOptions.Presets)
+			{
+				presets.Items.Add(preset.PresetName);
+			}
+			presets.SelectedIndex = 1;
+			//SetEncodingOptions(EncodingOptions.Custom720);
 		}
 
 		private async void OnStartConversion(object sender, EventArgs e)
@@ -69,116 +69,116 @@ namespace FFGUI
 				return;
 			}
 			var advancedOptions = GetEncodingOptions();
-            if (advancedOptions == null)
-            {
-                return;
-            }
-            try
-            {
-                toolStripStatusLabel1.Text = "Converting...";
-                toolStripProgressBar1.Visible = true;
+			if (advancedOptions == null)
+			{
+				return;
+			}
+			try
+			{
+				toolStripStatusLabel1.Text = "Converting...";
+				toolStripProgressBar1.Visible = true;
 
-                bool success = false;
-                if (checkBoxBatchMode.Checked)
-                {
-                    string outputFormat = conversionFormats.SelectedItem.ToString();
-                    success = await FFWrapper.StartBatchConversion(inputFileName.Text, outputFileName.Text, outputFormat, advancedOptions);
-                }
-                else
-                {
-                    success = await FFWrapper.StartConversion(inputFileName.Text, outputFileName.Text, advancedOptions);
-                }
-                var messageBoxMessage = String.Format("The conversion completed successfully. The result is saved at: \"{0}\".", outputFileName.Text);
-                var toolStripMessage = "Ready";
-                if (!success)
-                {
-                    toolStripMessage = "Conversion Failed";
-                    if (checkBoxBatchMode.Checked)
-                    {
-                        messageBoxMessage = "Not all files could be converted succesfully. Please review the log and try again.";
-                    }
-                    else
-                    {
-                        messageBoxMessage = "Something went wrong during conversion. Please try again.";
-                    }
-                }
-                toolStripStatusLabel1.Text = toolStripMessage;
-                toolStripProgressBar1.Visible = false;
-                MessageBox.Show(this, messageBoxMessage, "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+				bool success = false;
+				if (checkBoxBatchMode.Checked)
+				{
+					string outputFormat = conversionFormats.SelectedItem.ToString();
+					success = await FFWrapper.StartBatchConversion(inputFileName.Text, outputFileName.Text, outputFormat, advancedOptions);
+				}
+				else
+				{
+					success = await FFWrapper.StartConversion(inputFileName.Text, outputFileName.Text, advancedOptions);
+				}
+				var messageBoxMessage = String.Format("The conversion completed successfully. The result is saved at: \"{0}\".", outputFileName.Text);
+				var toolStripMessage = "Ready";
+				if (!success)
+				{
+					toolStripMessage = "Conversion Failed";
+					if (checkBoxBatchMode.Checked)
+					{
+						messageBoxMessage = "Not all files could be converted succesfully. Please review the log and try again.";
+					}
+					else
+					{
+						messageBoxMessage = "Something went wrong during conversion. Please try again.";
+					}
+				}
+				toolStripStatusLabel1.Text = toolStripMessage;
+				toolStripProgressBar1.Visible = false;
+				MessageBox.Show(this, messageBoxMessage, "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
-        
+		
 		private void OnBrowseInputFile(object sender, EventArgs e)
 		{
-            if (checkBoxBatchMode.Checked)
-            {
-                folderBrowserDialog1.SelectedPath = inputFileName.Text;
-                var result = folderBrowserDialog1.ShowDialog(this);
-                if (result == DialogResult.OK)
-                {
-                    var fileName = folderBrowserDialog1.SelectedPath;
-                    inputFileName.Text = fileName;
-                    outputFileName.Text = fileName + " output";
-                }
-            }
-            else
-            {
-                openFileDialog1.FileName = inputFileName.Text;
-                var result = openFileDialog1.ShowDialog(this);
-                if (result == DialogResult.OK)
-                {
-                    var fileName = openFileDialog1.FileName;
-                    inputFileName.Text = fileName;
-                    fileName = fileName.Substring(0, fileName.LastIndexOf('.'));
-                    outputFileName.Text = fileName + ".mp4";
-                }
-            }
+			if (checkBoxBatchMode.Checked)
+			{
+				folderBrowserDialog1.SelectedPath = inputFileName.Text;
+				var result = folderBrowserDialog1.ShowDialog(this);
+				if (result == DialogResult.OK)
+				{
+					var fileName = folderBrowserDialog1.SelectedPath;
+					inputFileName.Text = fileName;
+					outputFileName.Text = fileName + " output";
+				}
+			}
+			else
+			{
+				openFileDialog1.FileName = inputFileName.Text;
+				var result = openFileDialog1.ShowDialog(this);
+				if (result == DialogResult.OK)
+				{
+					var fileName = openFileDialog1.FileName;
+					inputFileName.Text = fileName;
+					fileName = fileName.Substring(0, fileName.LastIndexOf('.'));
+					outputFileName.Text = fileName + ".mp4";
+				}
+			}
 		}
 
 		private void OnBrowseOutputFile(object sender, EventArgs e)
 		{
-            if (checkBoxBatchMode.Checked)
-            {
-                folderBrowserDialog1.SelectedPath = outputFileName.Text;
-                var result = folderBrowserDialog1.ShowDialog(this);
-                if (result == DialogResult.OK)
-                {
-                    outputFileName.Text = folderBrowserDialog1.SelectedPath;
-                }
-            }
-            else
-            {
-                saveFileDialog1.FileName = outputFileName.Text;
-                var result = saveFileDialog1.ShowDialog(this);
-                if (result == DialogResult.OK)
-                {
-                    outputFileName.Text = saveFileDialog1.FileName;
-                }
-            }
+			if (checkBoxBatchMode.Checked)
+			{
+				folderBrowserDialog1.SelectedPath = outputFileName.Text;
+				var result = folderBrowserDialog1.ShowDialog(this);
+				if (result == DialogResult.OK)
+				{
+					outputFileName.Text = folderBrowserDialog1.SelectedPath;
+				}
+			}
+			else
+			{
+				saveFileDialog1.FileName = outputFileName.Text;
+				var result = saveFileDialog1.ShowDialog(this);
+				if (result == DialogResult.OK)
+				{
+					outputFileName.Text = saveFileDialog1.FileName;
+				}
+			}
 		}
 
 		private EncodingOptions GetEncodingOptions()
 		{
-		    var res = String.Empty;
-            var resText = videoResolution.Text;
-		    var p = resText.Split('x');
-            if (p.Length > 1)
-            {
-                uint a, b;
-                if (UInt32.TryParse(p[0], out a) && UInt32.TryParse(p[1], out b))
-                {
-                    res = a + "x" + b;
-                }
-            }
-            if ((!String.IsNullOrEmpty(resText)) && String.IsNullOrEmpty(res))
-            {
-                MessageBox.Show(this, String.Format("\"{0}\" is an invalid resolution. Please enter one in the format of \"1920x1080\" or leave the field blank", resText), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return null;
-            }
+			var res = String.Empty;
+			var resText = videoResolution.Text;
+			var p = resText.Split('x');
+			if (p.Length > 1)
+			{
+				uint a, b;
+				if (UInt32.TryParse(p[0], out a) && UInt32.TryParse(p[1], out b))
+				{
+					res = a + "x" + b;
+				}
+			}
+			if ((!String.IsNullOrEmpty(resText)) && String.IsNullOrEmpty(res))
+			{
+				MessageBox.Show(this, String.Format("\"{0}\" is an invalid resolution. Please enter one in the format of \"1920x1080\" or leave the field blank", resText), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return null;
+			}
 			var options = new EncodingOptions
 			{
 				IncludeVideo =includeVideo.Checked,
@@ -191,7 +191,7 @@ namespace FFGUI
 				AudioSampleRate = audioSamplerate.Text,
 				AudioBitrate = audioBitrate.Text,
 				AudioChannels = audioChannels.Text,
-                AudioScaleQuality = audioScaleQuality.Text,
+				AudioScaleQuality = audioScaleQuality.Text,
 			};
 
 			return options;
@@ -208,18 +208,18 @@ namespace FFGUI
 			includeAudio.Checked = options.IncludeAudio;
 			audioSamplerate.Text = options.AudioSampleRate;
 			audioBitrate.Text = options.AudioBitrate;
-            audioChannels.Text = options.AudioChannels;
-            audioScaleQuality.Text = options.AudioScaleQuality;
+			audioChannels.Text = options.AudioChannels;
+			audioScaleQuality.Text = options.AudioScaleQuality;
 		}
 
-        private void OnSelectPreset(object sender, EventArgs e)
-        {
-            SetEncodingOptions(EncodingOptions.Presets[presets.SelectedIndex]);
-        }
+		private void OnSelectPreset(object sender, EventArgs e)
+		{
+			SetEncodingOptions(EncodingOptions.Presets[presets.SelectedIndex]);
+		}
 
-        private void OnChangeBatchMode(object sender, EventArgs e)
-        {
-            conversionFormats.Visible = checkBoxBatchMode.Checked;
-        }
+		private void OnChangeBatchMode(object sender, EventArgs e)
+		{
+			conversionFormats.Visible = checkBoxBatchMode.Checked;
+		}
 	}
 }
--- FFGUI/FFMPEG-CSWrapper/FFWrapper.cs
+++ FFGUI/FFMPEG-CSWrapper/FFWrapper.cs
@@ -11,100 +11,133 @@ using System.Diagnostics;
 
 namespace FFMPEG_CSWrapper
 {
-    public static class FFWrapper
-    {
-        public static Task<bool> StartConversion(string inputFile, string outputFile, EncodingOptions advancedOptions)
-        {
-            var tcs = new TaskCompletionSource<bool>();
+	public static class FFWrapper
+	{
+		public static bool GenerateThumbnail(string inputFile, string outputFile, string timeIndex = "00:00:01.000")
+		{
+			SimpleLogger.LogMessage(String.Format("Generating Thumbnail for file: \"{0}\" --> \"{1}\", using time index: {2}", inputFile, outputFile, timeIndex));
 
-            try
-            {
-                SimpleLogger.LogMessage(String.Format("Starting Conversion: \"{0}\" --> \"{1}\"", inputFile, outputFile));
+			var commandArguments = String.Format("-i \"{0}\" -ss {2} -f image2 -vframes 1 \"{1}\"", inputFile, outputFile, timeIndex);
 
-                var commandLineArguments = String.Format("-i \"{0}\" {2} \"{1}\"", inputFile, outputFile, advancedOptions.ToString());
-                SimpleLogger.LogMessage("Using arguments: " + commandLineArguments);
+			SimpleLogger.LogMessage("Using arguments: " + commandArguments);
 
-                var ffmpeg = ConfigurationManager.AppSettings["FFMPEG_PATH"];
-                var procStartInfo = new ProcessStartInfo(ffmpeg, commandLineArguments)
-                {
-                    //*
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    //*/
+			var success = false;
+			try
+			{
+				var ffmpeg = ConfigurationManager.AppSettings["FFMPEG_PATH"];
+				var procStartInfo = new ProcessStartInfo(ffmpeg, commandArguments)
+				{
+					CreateNoWindow = true,
+					UseShellExecute = false,
+					WindowStyle = ProcessWindowStyle.Hidden,
 
-                    ErrorDialog = true,
-                };
-                //var p = Process.Start(procStartInfo);
-                var p = new Process()
-                {
-                    StartInfo = procStartInfo,
-                    EnableRaisingEvents = true,
-                };
+					ErrorDialog = true,
+				};
+				var p = Process.Start(procStartInfo);
+				p.WaitForExit();
+				success = true;
+			}
+			catch (Exception ex)
+			{
+				SimpleLogger.LogMessage("Exception in FFWrapper.GenerateThumbnail: " + ex.Message, "Exceptions");
+				success = false;
+			}
+			
+			return success;
+		}
 
-                //*
-                p.Exited += (o, args) =>
-                {
-                    SimpleLogger.LogMessage("Conversion done");
-                    p.Dispose();
-                    tcs.TrySetResult(true);
-                };
-                p.OutputDataReceived += (o, args) =>
-                {
-                    SimpleLogger.LogMessage(args.Data);
-                };
-                p.ErrorDataReceived += (o, args) =>
-                {
-                    SimpleLogger.LogMessage(args.Data, "Errors");
-                };
-                //*/
-                if(p.Start())
-                {
-                    p.BeginOutputReadLine();
-                    p.BeginErrorReadLine();
-                }
-                else
-                {
-                    tcs.TrySetException(new InvalidOperationException("Failed to start process"));
-                }
+		public static Task<bool> StartConversion(string inputFile, string outputFile, EncodingOptions advancedOptions)
+		{
+			var tcs = new TaskCompletionSource<bool>();
 
-                return tcs.Task;
-            }
-            catch (Exception ex)
-            {
-                SimpleLogger.LogMessage("Exception in FFWrapper.StartConversion: " + ex.Message, "Exceptions");
-                tcs.TrySetException(ex);
-                return tcs.Task;
-            }
-        }
+			try
+			{
+				SimpleLogger.LogMessage(String.Format("Starting Conversion: \"{0}\" --> \"{1}\"", inputFile, outputFile));
 
-        public static async Task<bool> StartBatchConversion(string inputFolder, string outputFolder, string outputFormat, EncodingOptions advancedOptions)
-        {
-            if(!Directory.Exists(outputFolder)) {
-                Directory.CreateDirectory(outputFolder);
-            }
+				var commandLineArguments = String.Format("-i \"{0}\" {2} \"{1}\"", inputFile, outputFile, advancedOptions.ToString());
+				SimpleLogger.LogMessage("Using arguments: " + commandLineArguments);
 
-            var result = new List<bool>();
-            var di = new DirectoryInfo(inputFolder);
-            var files = di.GetFiles();
-            foreach(var file in files)
-            {
-                var status = false;
-                try
-                {
-                    var inName = file.FullName;
-                    var outName = String.Format("{0}\\{1}.{2}", outputFolder, file.Name, outputFormat);
-                    status = await StartConversion(inName, outName, advancedOptions);
-                }
-                catch (Exception ex)
-                {
-                    SimpleLogger.LogMessage("Exception in FFWrapper.StartBatchConversion: " + ex.Message);
-                }
-                result.Add(status);
-            }
-            return result.All(b => b);
-        }
-    }
+				var ffmpeg = ConfigurationManager.AppSettings["FFMPEG_PATH"];
+				var procStartInfo = new ProcessStartInfo(ffmpeg, commandLineArguments)
+				{
+					//*
+					CreateNoWindow = true,
+					UseShellExecute = false,
+					WindowStyle = ProcessWindowStyle.Hidden,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					//*/
+
+					ErrorDialog = true,
+				};
+				//var p = Process.Start(procStartInfo);
+				var p = new Process()
+				{
+					StartInfo = procStartInfo,
+					EnableRaisingEvents = true,
+				};
+
+				//*
+				p.Exited += (o, args) =>
+				{
+					SimpleLogger.LogMessage("Conversion done");
+					p.Dispose();
+					tcs.TrySetResult(true);
+				};
+				p.OutputDataReceived += (o, args) =>
+				{
+					SimpleLogger.LogMessage(args.Data);
+				};
+				p.ErrorDataReceived += (o, args) =>
+				{
+					SimpleLogger.LogMessage(args.Data, "Errors");
+				};
+				//*/
+				if(p.Start())
+				{
+					p.BeginOutputReadLine();
+					p.BeginErrorReadLine();
+				}
+				else
+				{
+					tcs.TrySetException(new InvalidOperationException("Failed to start process"));
+				}
+
+				return tcs.Task;
+			}
+			catch (Exception ex)
+			{
+				SimpleLogger.LogMessage("Exception in FFWrapper.StartConversion: " + ex.Message, "Exceptions");
+				tcs.TrySetException(ex);
+				return tcs.Task;
+			}
+		}
+
+		public static async Task<bool> StartBatchConversion(string inputFolder, string outputFolder, string outputFormat, EncodingOptions advancedOptions)
+		{
+			if(!Directory.Exists(outputFolder)) {
+				Directory.CreateDirectory(outputFolder);
+			}
+
+			var result = new List<bool>();
+			var di = new DirectoryInfo(inputFolder);
+			var files = di.GetFiles();
+			foreach(var file in files)
+			{
+				var status = false;
+				try
+				{
+					var inName = file.FullName;
+					var outName = String.Format("{0}\\{1}.{2}", outputFolder, file.Name, outputFormat);
+					status = await StartConversion(inName, outName, advancedOptions);
+				}
+				catch (Exception ex)
+				{
+					SimpleLogger.LogMessage("Exception in FFWrapper.StartBatchConversion: " + ex.Message);
+				}
+				result.Add(status);
+			}
+			return result.All(b => b);
+		}
+	}
 }
